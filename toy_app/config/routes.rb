4214fe8989c0c56d95393de28d77e4dabe7966a8
Darko Veljanoski <veldarko@yahoo.com>
2018-01-14 22:05:26 +0100

    Finish toy app

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   root 'application#hello'
+end
+
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
+end
+
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
 end
